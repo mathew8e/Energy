46b5b5191cac4beae3eaa8b1f7142eb1913b0bfc
mathew8e <123kumatan@gmail.com>
2021-06-13 19:55:18 +0200

    changed to async

--- EnergyScreenTimelapse/EnergyScreenTimelapse.py
+++ EnergyScreenTimelapse/EnergyScreenTimelapse.py
@@ -2,66 +2,60 @@ import cv2
 import numpy as np
 import pyautogui
 from time import sleep
-import eel
+import async_eel
 import io
 import sys
 import asyncio
 
-exitcode = 0
+loop = asyncio.get_event_loop()
 
 def close_callback(route, websockets):
-	global exitcode, loop
 	exitcode = True
 	if not websockets:
 		print('Bye!')
-		loop.stop()
+		loop.close()
 		sys.exit()
 		exit()
 
 
-eel.init('web')
 
 screen_size = (1920,1080)
 fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-out = cv2.VideoWriter('output/output.mp4',fourcc,20.0,(screen_size))
+	
 
-@eel.expose
-def start(lapsName):
-	global out
-	print(lapsName)
+
+@async_eel.expose
+async def frameLoop(speed, lapsName):
+	global fourcc, screen_size
 	out = cv2.VideoWriter('output/'+str(lapsName)+'.mp4',fourcc,20.0,(screen_size))
+	while 1:
+		img = pyautogui.screenshot()
+		frame = np.array(img)
+		frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+		out.write(frame)
+		#cv2.imshow("show",frame)
+		print("Frame taken...")
+		await asyncio.sleep(int(speed))
 
 
-@eel.expose
-def frameLoop(s):
-	global out, loop
-	img = pyautogui.screenshot()
-	frame = np.array(img)
-	frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-	out.write(frame)
-	frameskip = 0
-	cv2.imshow("show",frame)
-	print("Frame taken...")
-	sleep(int(s))
-
-def asyncfunc(s):
-	global loop
-	print("start")
-	loop = asyncio.get_event_loop(s)	
-	loop.run_forever()
-
-@eel.expose
-def stop():
-	global exitcode
+@async_eel.expose
+async def stop():
 	sleep(1)
 	print("stopping..")
+	loop.close()
 	sys.exit()
-	exit()
-@eel.expose
+	await exit()
+
+@async_eel.expose
 def write(a):
 	sleep(0.1)
 	print(a)
 
+async def main():
+	async_eel.init('web')
+	await async_eel.start('index.html', size=(700, 600), close_callback=close_callback)
 
-eel.start('index.html', size=(700, 600), close_callback=close_callback)
+if __name__ == "__main__":
+    asyncio.run_coroutine_threadsafe(main(), loop)
+    loop.run_forever()
 
--- EnergyScreenTimelapse/web/script.js
+++ EnergyScreenTimelapse/web/script.js
@@ -2,9 +2,8 @@
 function collectData(){
     var range = document.getElementById("range").value
     var lapsName = document.getElementById("filename").value
-    eel.start(lapsName) 
 
-    eel.asyncfunc(range)
+    eel.frameLoop(range, lapsName)
     
 }
 function stop(){
